3cb5b65ee983dd2ba3deae3d43e1470767e326b2
jonivanderhaeghe <joni.vanderhaeghe@vtibrugge.be>
2025-03-11 13:57:35 +0100

    Delete ChangeBackgroundColor directory

--- ChangeBackgroundColor/JS/script.js
+++
@@ -1,6 +0,0 @@
-function generateRandomBackgroundColor() {
-    const color= "#" + Math.floor(Math.random()*16777215).toString(16);
-    document.body.style.backgroundColor = color;
-    console.log("hello")
-}
-document.getElementById("changeColorBtn").addEventListener("click", generateRandomBackgroundColor);
